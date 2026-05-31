55209eb34c849c0fb71d34f7c6bcd3adc34a4512
PyDiamond <kevinchuyes14@gmail.com>
2026-05-31 16:31:52 -0500

    Corregido nombre del método en pruebas unitarias

--- src/test/java/test/BattleSystemTest.java
+++ src/test/java/test/BattleSystemTest.java
@@ -27,7 +27,7 @@ public class BattleSystemTest {
         Pokemon squirtle = PokemonFactory.createPokemon(7);
 
         boolean result =
-                battleSystem.playerAttacksFirst(pikachu, squirtle);
+                battleSystem.playerAttackFirst(pikachu, squirtle);
 
         assertTrue(result);
     }
@@ -43,10 +43,10 @@ public void sameSpeedShouldHaveConsistentOrder() {
     Pokemon mew2 = PokemonFactory.createPokemon(151);
 
     boolean firstResult =
-            battleSystem.playerAttacksFirst(mew1, mew2);
+            battleSystem.playerAttackFirst(mew1, mew2);
 
     boolean secondResult =
-            battleSystem.playerAttacksFirst(mew1, mew2);
+            battleSystem.playerAttackFirst(mew1, mew2);
 
     assertEquals(firstResult, secondResult);
     }
